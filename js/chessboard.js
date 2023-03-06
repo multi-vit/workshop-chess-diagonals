3e2bfc152142e0ca742aeba06fc56aac9478d4d5
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-03-06 11:36:15 +0000

    Complete draw function to draw board

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,20 +1,32 @@
 export default {
-	draw,
-	highlight
+  draw,
+  highlight,
 };
-
 
 // ****************************
 
+var origBoardEl;
+
 function draw(boardEl) {
-	// TODO: draw the chessboard, 8 rows (divs)
-	// of 8 tiles (divs) each, inserting all DOM
-	// elements into `boardEl` div
+  origBoardEl = boardEl;
+  for (let i = 0; i < 8; i++) {
+    let rowEl = document.createElement("div");
+    for (let j = 0; j < 8; j++) {
+      let tileEl = document.createElement("div");
+      rowEl.appendChild(tileEl);
+    }
+    boardEl.appendChild(rowEl);
+  }
 }
 
 function highlight(tileEl) {
-	// TODO: clear previous highlights (if any) and
-	// then find the tiles in the two diagonals
-	// (major and minor) that `tileEl` belongs to,
-	// to highlight them via CSS class "highlighted"
+  var tiles = origBoardEl.querySelectorAll("div > div");
+  for (let el of tiles) {
+    el.classList.remove("highlighted");
+  }
+  if (tileEl) {
+    let rowEl = tileEl.parentNode;
+    let tileRowIdx = [...origBoardEl.childNodes].findIndex((el) => el == rowEl);
+    let tileColIdx = [...rowEl.childNodes].findIndex((el) => el == tileEl);
+  }
 }
